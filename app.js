a59fb27cce442a62351d1f4c4d7487b23a9cfe1e
xu <695540791@qq.com>
2014-08-07 15:45:51 +0800

    加入了html2canvas初步完成dom转canvas然后分散的任务

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
  */
 var express = require("express");
 var path = require("path");
+
 var mongoose = require("mongoose");
+
 mongoose.connect("mongodb://127.0.0.1/myself",function(err){
     if(err){
         console.log("数据库连接失败！\n"+err);
@@ -21,17 +23,18 @@ mongoose.model("book",Book);
 var Book = mongoose.model("book");
 var book1 = new Book({name:"深入浅出nodejs",describe:"好书"});
 book1.save();
-express.static(path.join(__dirname,"public"));
+
 var app = express(function(){
 
 });
+app.use('/public',express.static(path.join(__dirname,'public')));
 //console.log(app.locals);
 app.set("views",path.join(__dirname,"view"));
 app.set("view engine","html");//这是设置默认引擎扩展名的，也就是说如果不写扩展名，默认添加的扩展名
 app.engine("html",require("ejs").renderFile);//这才是真正的设置模板引擎
 
 
-app.get("/sendFile", function (req, res) {
+app.get("/", function (req, res) {
    res.sendfile("view/hello.html");
 }).listen(3000);
 app.get("/renderCallback",function(req,res){
@@ -47,6 +50,6 @@ app.get("/readbook", function (req, res) {
         res.render("main",books);
     });
 app.get("/", function (req, res) {
-
+    res.render("hello.html");
 });
 });
